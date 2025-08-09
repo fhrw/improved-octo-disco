1b966519ed7fddc204878037dbbfd7ee11ea64a6
Felix Watson <felix.dork@gmail.com>
2025-08-09 14:11:10 +1000

    added hoogle

--- custom-file.el
+++ custom-file.el
@@ -5,9 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7235b77f371f46cbfae9271dce65f5017b61ec1c8687a90ff30c6db281bfd6b7" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))
+   '("c5975101a4597094704ee78f89fb9ad872f965a84fb52d3e01b9102168e8dc40" "7235b77f371f46cbfae9271dce65f5017b61ec1c8687a90ff30c6db281bfd6b7" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))
  '(package-selected-packages
-   '(consult-eglot flycheck purescript-mode ormolu haskell-mode org-pomodoro slime envrc paredit ace-window corfu eldoc-box evil-surround evil-collection evil consult c-eldoc cape color-theme-sanityinc-tomorrow direnv general key-chord orderless swiper undo-fu vertico)))
+   '(consult-hoogle consule-hoogle consult-eglot flycheck purescript-mode ormolu haskell-mode org-pomodoro slime envrc paredit ace-window corfu eldoc-box evil-surround evil-collection evil consult c-eldoc cape color-theme-sanityinc-tomorrow direnv general key-chord orderless swiper undo-fu vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
